55e0b19ad2d0f85c3e097da05823bdafd4cdbf36
Marcus Jensen <marcusjensen1515@gmail.com>
2020-04-11 16:31:12 -0700

    import ejs files in order to condence code

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = new express();
 const path = require('path');
+const ejs = require('ejs');
+app.set('view engine', 'ejs'); //this is telling express to use ejs as our templating engine. any file ending in .ejs should be rendered with the EJS package
 //tells it that all static assets live in the public library
 app.use(express.static('public'));
 app.listen(4000,() => {
@@ -8,17 +10,21 @@ app.listen(4000,() => {
 })
 
 app.get('/', (req,res) => {
-  res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+  // res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+  res.render('index');
 });
 
 app.get('/about', (req,res) => {
-  res.sendFile(path.resolve(__dirname,'pages/about.html'));
+  // res.sendFile(path.resolve(__dirname,'pages/about.html'));
+  res.render('about');
 });
 
 app.get('/contact',(req,res) => {
-  res.sendFile(path.resolve(__dirname,'pages/contact.html'));
+  // res.sendFile(path.resolve(__dirname,'pages/contact.html'));
+  res.render('contact');
 });
 
 app.get('/post',(req,res) => {
-  res.sendFile(path.resolve(__dirname,'pages/post.html'));
+  // res.sendFile(path.resolve(__dirname,'pages/post.html'));
+  res.render('post');
 });
