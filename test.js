9112bca30a8d8430bde974017da35283e4758ede
Marcus Jensen <marcusjensen1515@gmail.com>
2020-04-26 12:59:33 -0700

    node test.js sucessfully allowing me to add blog posts. Need to make sure MongoDB Database is running before trying to write data into it

--- test.js
+++ test.js
@@ -1,2 +1,11 @@
-const mongoose = require('mongoose');
-const BlogPost = require('./models/BlogPost');
+const mongoose = require('mongoose')
+const BlogPost = require('./models/BlogPost')
+
+mongoose.connect('mongodb://localhost/my_database', {useNewUrlParser: true});
+
+BlogPost.create({
+  title: 'Guide to saving on energy bills',
+  body: 'Using less energy is a great way to save money on energy bills.'
+}, (error, blogpost) =>{
+  console.log(error,blogpost);
+})
