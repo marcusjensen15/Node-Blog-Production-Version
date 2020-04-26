ba0266de5bcbaed3d28ed2ea3c32c94c4468b691
Marcus Jensen <marcusjensen1515@gmail.com>
2020-04-26 12:46:20 -0700

    add mongoose to project

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = new express();
 const path = require('path');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/my_database', {useNewUrlParser: true});
 app.set('view engine', 'ejs'); //this is telling express to use ejs as our templating engine. any file ending in .ejs should be rendered with the EJS package
 //tells it that all static assets live in the public library
 app.use(express.static('public'));
---
+++ test.js
@@ -0,0 +1,2 @@
+const mongoose = require('mongoose');
+const BlogPost = require('./models/BlogPost');
