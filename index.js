219550b164bf8c540a459ffca12d8c39be93452f
Marcus Jensen <marcusjensen1515@gmail.com>
2020-05-07 14:58:20 -0700

    add min text area to edit posts. this prevents you from submitting blank body fields

--- index.js
+++ index.js
@@ -50,6 +50,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(customMiddleWare);
 app.use('/posts/store',validateMiddleWare);
+
 app.use(expressSession({
   secret: 'keybord cat'
 }))
